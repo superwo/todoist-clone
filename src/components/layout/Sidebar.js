f740fcfcbcc91e96a0a64243b27ed9b37722d935
Radu <avadanii@tiscali.it>
2019-10-10 09:53:39 +0300

    created context for projects

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar
 } from 'react-icons/fa';
+import { useSelectedProjectValue } from '../../context';
+import Projects from '../Projects';
 
 const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className='sidebar' data-testid='sidebar'>
       <ul className='sidebar__generic'>
@@ -35,8 +41,7 @@ const Sidebar = () => {
         </span>
         <h2>Projects</h2>
       </div>
-      <ul className='sidebar__projects'>Projects will be here!</ul>
-      Add Project!
+      <ul className='sidebar__projects'>{showProjects && <Projects />}</ul>
     </div>
   );
 };
